bf0bc7b352485ac7da8768074efe71417f65ba62
Excavator <pilot0613@gmail.com>
2017-07-14 07:50:57 +0000

    rm @current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,5 @@ class ApplicationController < ActionController::Base
     @devise_mapping ||= Devise.mappings[:user]
   end
   
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
   
 end
--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -29,7 +29,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def self.provides_callback_for(provider)
     class_eval %Q{
       def #{provider}
-        @user = User.find_for_oauth(env["omniauth.auth"], @current_user)
+        @user = User.find_for_oauth(env["omniauth.auth"], current_user)
         
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication
@@ -50,7 +50,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def after_sign_in_path_for(resource)
     auth = request.env['omniauth.auth']
     @identity = Identity.find_for_oauth(auth)
-    @user = User.find(@current_user.id)
+    @user = User.find(current_user.id)
     
     if @user.persisted?
       if @identity.provider == "kakao"

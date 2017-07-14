3804eead59b386460be2354d78b654cc4f0e1390
Excavator <pilot0613@gmail.com>
2017-07-14 07:40:28 +0000

    hihi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,9 @@ class ApplicationController < ActionController::Base
     @devise_mapping ||= Devise.mappings[:user]
   end
   
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+  
 end

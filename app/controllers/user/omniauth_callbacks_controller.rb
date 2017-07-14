b61e90f1dd596c97212d4b3c0d54fed7c2c2b97f
Excavator <pilot0613@gmail.com>
2017-07-14 07:46:44 +0000

    @current_user

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -29,7 +29,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def self.provides_callback_for(provider)
     class_eval %Q{
       def #{provider}
-        @user = User.find_for_oauth(env["omniauth.auth"], current_user)
+        @user = User.find_for_oauth(env["omniauth.auth"], @current_user)
         
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication
